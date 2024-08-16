8b20fe5d253ff99c9637b2f842ff8296d272af5b
ilya picenko <pitsenko.ilya@mail.ru>
2024-08-16 20:00:35 +0300

    init

---
+++ src/main/java/com/vianny/region_restrict/RegionRestrictModClient.java
@@ -0,0 +1,10 @@
+package com.vianny.region_restrict;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RegionRestrictModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
